9ead5f5f45cf9471c4032e64bff5bf863496781d
gena <gepota1@.com>
2024-03-13 22:06:24 +0300

    DZ_2/DZ_3/ is done

---
+++ DZ_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете 
+// его элементы, начиная с конца. Использовать 
+// рекурсию, не использовать циклы.
+
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+
+void PrintReversedArray(int[] array)
+{
+    if (array == null || array.Length == 0) return;
+
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+// Пример использования функции
+int[] numbers = new int[] { 1, 2, 5, 0, 10, 34 };
+PrintReversedArray(numbers);
+
